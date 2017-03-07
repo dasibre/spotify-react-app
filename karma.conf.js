bea875f4cdc1289f5b87c9ec7877f56c9cb4f06f
James Naadjie <jnaadjie@gmail.com>
2017-03-07 06:13:00 -0500

    working karma test configuration

--- karma.conf.js
+++ karma.conf.js
@@ -1,13 +1,22 @@
+const entry1 = 'test/**/*_spec.jsx';
+const entry2 = 'test/**/*_spec.js';
+const webpackConfig = require('./webpack.config');
+
 module.exports = function setKarmaConfig(config) {
   config.set({
-    basePath: '',
-    frameworks: ['mocha'],
+    basePath: './',
+    frameworks: ['mocha', 'chai'],
     files: [
-      './src/*js',
-      './test/**/*.js'
+			{pattern: entry1, watched: false},
+			{pattern: entry2, watched: false}
     ],
+    exclude: ['/test/helpers/*'],
     preprocessors: {
+      [entry1]: ['webpack'],
+      [entry2]: ['webpack']
     },
+    webpack: webpackConfig,
+    webpackMiddleware: {noInfo: true},
     reporters: ['progress'],
     port: 9876,
     colors: true,
